a3490f8b638ca71d1f070089096703fd5c40031a
Slyces <slyces.coding@gmail.com>
2018-12-19 17:45:05 +0000

    suppression de fichiers inutiles

--- functions/get_functions_list.sh
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env/bash
-
-# download the list
-if [ ! -f functions_list.txt ]; then
-    wget -q -O - https://www.sfu.ca/~ssurjano/Code/ | grep -iP '\"[\w\d]+?m\.html\"' | sed -E 's/^.*\"(.*?)m\.html\".*$/\1/' > functions_list.txt
-fi
-# get only the matlab
